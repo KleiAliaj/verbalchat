6c3f821454f7b8a63b02a3e3bdf9fb42d365beaf
KleiAliaj <40433677+KleiAliaj@users.noreply.github.com>
2023-12-15 18:16:19 +0100

    Update Skills.js

--- components/Home/Skills.js
+++ components/Home/Skills.js
@@ -1,54 +1,159 @@
+import {
+  Box,
+  Grid,
+  Heading,
+  Icon,
+  Stack,
+  VStack,
+  Text,
+  Flex,
+  Button,
+} from "@chakra-ui/react";
 import React from "react";
-import Image from "next/image";
-import { Box, Flex, IconButton } from "@chakra-ui/react";
+import NextLink from "next/link";
 import BaseLayout from "../Wrapper/BaseLayout";
-import BaseText from "../Wrapper/BaseText";
-import { skillDetails } from "./__skillsDetails";
+import Skills from "./Skills";
+import Timeline from "./Timeline";
+import SupportedLanguages from "./SupportedLanguages";
+import { productCardDetails } from "./__productCardDetails";
 
-const Skills = () => {
+import { IoIosRocket } from "react-icons/io";
+
+const Home = () => {
   return (
     <React.Fragment>
-      <BaseLayout id="skills">
-        <BaseText
-          id="skills-header-wrapper"
-          firstTitle="Connecting With"
-          secondTitle=" Open Source"
-          textIcon="https://ik.imagekit.io/ayushsoni1010/Website/skills?ik-sdk-version=javascript-1.4.3&updatedAt=1669666366824"
-          leftSpacing={20}
-          topSpacing={-4}
-        />
-        <Box
-          boxShadow="2xl"
-          borderRadius="3xl"
-          my="10"
-          bgColor="white"
-          id="skills-wrapper"
+      <BaseLayout mt="10" id="stack">
+        <Heading
+          fontSize={{
+            base: "5xl",
+            md: "5xl",
+            lg: "5xl",
+            sm: "3xl",
+            xs: "3xl",
+          }}
+          fontWeight="bold"
+          textAlign="center"
         >
-          <Flex
-            gap={{ base: 16, lg: 16, md: 16, sm: 14, xs: 10 }}
-            wrap="wrap"
-            p={{ base: 10, lg: 10, md: 10, sm: 10, xs: 0 }}
-            pt={{ base: 10, lg: 10, md: 10, sm: 10, xs: 10 }}
-            pb={{ base: 10, lg: 10, md: 10, sm: 10, xs: 10 }}
-            my={{ base: 0, lg: 0, md: 0, sm: 4, xs: 10 }}
-            alignItems="center"
-            justify="center"
+          Verbal offers:
+        </Heading>
+        {/* <Text
+          textAlign="center"
+          fontSize={{ base: "md", md: "md", lg: "md", sm: "sm", xs: "sm" }}
+          my="5"
+          mx={{ lg: 60, md: 60, sm: 0, xs: 0 }}
+        >
+          From designing the interface to implementing your fully featured
+          application - I can provide the full product design process from an
+          idea to a finished hiqh quality app, polished with fantastic design
+          and running on state of the art software.
+        </Text> */}
+        <Box align="center" my={{ base: 20, lg: 20, md: 20, sm: 14, xs: 14 }}>
+          <Grid
+            display={{
+              base: "grid",
+              md: "grid",
+              lg: "grid",
+              sm: "block",
+              xs: "block",
+            }}
+            templateColumns="repeat(2,1fr)"
+            maxW="3xl"
+            gap="20"
           >
-            {skillDetails.map((item) => (
-              <Box key={item.key}>
-                <Image
-                  width="70"
-                  height="70"
-                  src={item.iconURL}
-                  alt={item.name}
-                  aria-label={item.name}
-                />
-              </Box>
-            ))}
-          </Flex>
+            {productCardDetails.map(
+              (item, index) =>
+                item?.stack && (
+                  <Stack
+                    key={index}
+                    spacing="3"
+                    align="center"
+                    mt={{ md: 0, lg: 0, sm: 10, xs: 10 }}
+                  >
+                    <Box
+                      w="60px"
+                      h="60px"
+                      borderRadius="full"
+                      align="center"
+                      bg={item?.bgColor}
+                      mb="2"
+                      boxShadow="2xl"
+                      transition={"ease-in-out"}
+                      transitionDuration="0.5s"
+                      _hover={{ boxShadow: "outline" }}
+                      cursor="pointer"
+                    >
+                      <Icon mt="5" w="5" h="5">
+                        {item?.svgIcon}
+                      </Icon>
+                    </Box>
+                    <Heading fontSize="xl">{item?.title}</Heading>
+                    <Text fontSize="sm">{item?.description}</Text>
+                  </Stack>
+                )
+            )}
+          </Grid>
         </Box>
+      </BaseLayout>
+      <Skills />
+   
+     
+      {/* <Timeline /> */}
+      <SupportedLanguages/>
+      <BaseLayout>
+        <Flex
+          mb={{ base: 14, lg: 14, md: 14, sm: 20, xs: 20 }}
+          p="10"
+          w="full"
+          borderRadius="10px"
+          color="black"
+          alignItems={"center"}
+          justify={"space-evenly"}
+          display={{
+            base: "flex",
+            md: "flex",
+            lg: "flex",
+            sm: "block",
+            xs: "block",
+          }}
+          bgGradient="radial-gradient(circle, #3889D2, #BDD4E7)">
+          <VStack alignItems={"flex-start"}>
+            <Text
+              fontSize={{
+                base: "4xl",
+                lg: "4xl",
+                md: "4xl",
+                sm: "2xl",
+                xs: "2xl",
+              }}
+              fontWeight={"bold"}
+            >
+              Let&apos;s Build Something Together
+            </Text>
+
+            <Text fontSize={"lg"}>
+              Turning ideas into real life products is my calling. Let&apos;s do
+              this.
+            </Text>
+          </VStack>
+          <NextLink href="/links" passHref>
+            <Button
+              leftIcon={<IoIosRocket />}
+              colorScheme="white"
+              color="white"
+              bg="black"
+              size={"lg"}
+              shadow={"dark-lg"}
+              _hover={{ shadow: "none" }}
+              variant="solid"
+              marginTop={{ base: 0, lg: 0, md: 0, sm: 4, xs: 4 }}
+            >
+              Connect With Us
+            </Button>
+          </NextLink>
+        </Flex>
       </BaseLayout>
     </React.Fragment>
   );
 };
-export default Skills;
+
+export default Home;
