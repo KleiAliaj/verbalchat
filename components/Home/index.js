99df06e6e190f76be18d0b6207340ed6c356e1ab
KleiAliaj <40433677+KleiAliaj@users.noreply.github.com>
2023-12-16 14:44:27 +0100

    Update index.js

--- components/Home/index.js
+++ components/Home/index.js
@@ -15,9 +15,8 @@ import BaseLayout from "../Wrapper/BaseLayout";
 import Skills from "./Skills";
 import Timeline from "./Timeline";
 import SupportedLanguages from "./SupportedLanguages";
-import  productCardDetails  from "./__productCardDetails";
+import { productCardDetails } from "./__productCardDetails";
 import PricingComponent from "./../pricing"
-
 import { IoIosRocket } from "react-icons/io";
 
 const Home = () => {
@@ -96,76 +95,7 @@ const Home = () => {
         </Box>
       </BaseLayout>
       <Skills />
-       <BaseLayout mt="24" id="product">
-        <Heading
-          fontSize={{
-            base: "5xl",
-            md: "5xl",
-            lg: "5xl",
-            sm: "3xl",
-            xs: "3xl",
-          }}
-          fontWeight="bold"
-          textAlign="center"
-        >
-          Product development, the easy way.
-        </Heading>
-        <Text
-          textAlign="center"
-          fontSize="md"
-          my="5"
-          mx={{ lg: 60, md: 60, sm: 0, xs: 0 }}
-        >
-          I provide you the following perks for your product development
-          process, and more.
-        </Text>
-        <Box align="center" my={{ base: 20, lg: 20, md: 20, sm: 10, xs: 10 }}>
-          <Grid
-            display={{
-              base: "grid",
-              md: "grid",
-              lg: "grid",
-              sm: "block",
-              xs: "block",
-            }}
-            templateColumns="repeat(2,1fr)"
-            gap="20"
-            maxW="3xl"
-          >
-            {productCardDetails.map(
-              (item, index) =>
-                !item?.stack && (
-                  <Stack
-                    key={index}
-                    spacing="3"
-                    align="center"
-                    mt={{ md: 0, lg: 0, sm: 10, xs: 10 }}
-                  >
-                    <Box
-                      w="60px"
-                      h="60px"
-                      borderRadius="full"
-                      align="center"
-                      bg={item.bgColor}
-                      mb="2"
-                      boxShadow="2xl"
-                      transition={"ease-in-out"}
-                      transitionDuration="0.5s"
-                      _hover={{ boxShadow: "outline" }}
-                      cursor="pointer"
-                    >
-                      <Icon mt="5" w="5" h="5">
-                        {item?.svgIcon}
-                      </Icon>
-                    </Box>
-                    <Heading fontSize="xl">{item?.title}</Heading>
-                    <Text fontSize="sm">{item?.description}</Text>
-                  </Stack>
-                )
-            )}
-          </Grid>
-        </Box>
-      </BaseLayout> 
+   
      
       {/* <Timeline /> */}
       <SupportedLanguages/>
