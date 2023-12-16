7bcb021cda340d9344f0f7e6d3019cb2a351e634
KleiAliaj <40433677+KleiAliaj@users.noreply.github.com>
2023-12-16 17:34:50 +0100

    Update index.js

--- components/Home/index.js
+++ components/Home/index.js
@@ -17,7 +17,6 @@ import Timeline from "./Timeline";
 import SupportedLanguages from "./SupportedLanguages";
 import { productCardDetails } from "./__productCardDetails";
  import { IoIosRocket } from "react-icons/io";
- import PricingComponent from "./PricingComponent";
 
 
 const Home = () => {
@@ -100,7 +99,6 @@ const Home = () => {
      
       {/* <Timeline /> */}
       <SupportedLanguages/>
-<PricingComponent/>
       <BaseLayout>
         <Flex
           mb={{ base: 14, lg: 14, md: 14, sm: 20, xs: 20 }}
