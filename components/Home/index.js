25fc5d64e155028611dd53bce90fcef8d7cfa56a
KleiAliaj <40433677+KleiAliaj@users.noreply.github.com>
2023-12-16 15:40:45 +0100

    Update index.js

--- components/Home/index.js
+++ components/Home/index.js
@@ -14,8 +14,8 @@ import NextLink from "next/link";
 import BaseLayout from "../Wrapper/BaseLayout";
 import Skills from "./Skills";
 import Timeline from "./Timeline";
-import SupportedLanguages from "./SupportedLanguages";
 import { productCardDetails } from "./__productCardDetails";
+
 import { IoIosRocket } from "react-icons/io";
 
 const Home = () => {
@@ -33,9 +33,9 @@ const Home = () => {
           fontWeight="bold"
           textAlign="center"
         >
-          Verbal offers:
+          The full product development stack.
         </Heading>
-        {/* <Text
+        <Text
           textAlign="center"
           fontSize={{ base: "md", md: "md", lg: "md", sm: "sm", xs: "sm" }}
           my="5"
@@ -45,7 +45,7 @@ const Home = () => {
           application - I can provide the full product design process from an
           idea to a finished hiqh quality app, polished with fantastic design
           and running on state of the art software.
-        </Text> */}
+        </Text>
         <Box align="center" my={{ base: 20, lg: 20, md: 20, sm: 14, xs: 14 }}>
           <Grid
             display={{
@@ -93,9 +93,77 @@ const Home = () => {
           </Grid>
         </Box>
       </BaseLayout>
-      <Skills />   
-      {/* <Timeline /> */}
-      <SupportedLanguages/>
+      <Skills />
+      <BaseLayout mt="24" id="product">
+        <Heading
+          fontSize={{
+            base: "5xl",
+            md: "5xl",
+            lg: "5xl",
+            sm: "3xl",
+            xs: "3xl",
+          }}
+          fontWeight="bold"
+          textAlign="center"
+        >
+          Product development, the easy way.
+        </Heading>
+        <Text
+          textAlign="center"
+          fontSize="md"
+          my="5"
+          mx={{ lg: 60, md: 60, sm: 0, xs: 0 }}
+        >
+          I provide you the following perks for your product development
+          process, and more.
+        </Text>
+        <Box align="center" my={{ base: 20, lg: 20, md: 20, sm: 10, xs: 10 }}>
+          <Grid
+            display={{
+              base: "grid",
+              md: "grid",
+              lg: "grid",
+              sm: "block",
+              xs: "block",
+            }}
+            templateColumns="repeat(2,1fr)"
+            gap="20"
+            maxW="3xl"
+          >
+            {productCardDetails.map(
+              (item, index) =>
+                !item?.stack && (
+                  <Stack
+                    key={index}
+                    spacing="3"
+                    align="center"
+                    mt={{ md: 0, lg: 0, sm: 10, xs: 10 }}
+                  >
+                    <Box
+                      w="60px"
+                      h="60px"
+                      borderRadius="full"
+                      align="center"
+                      bg={item.bgColor}
+                      mb="2"
+                      boxShadow="2xl"
+                      transition={"ease-in-out"}
+                      transitionDuration="0.5s"
+                      _hover={{ boxShadow: "outline" }}
+                      cursor="pointer"
+                    >
+                      <Icon mt="5" w="5" h="5">
+                        {item?.svgIcon}
+                      </Icon>
+                    </Box>
+                    <Heading fontSize="xl">{item?.title}</Heading>
+                    <Text fontSize="sm">{item?.description}</Text>
+                  </Stack>
+                )
+            )}
+          </Grid>
+        </Box>
+      </BaseLayout>
       <BaseLayout>
         <Flex
           mb={{ base: 14, lg: 14, md: 14, sm: 20, xs: 20 }}
@@ -112,7 +180,8 @@ const Home = () => {
             sm: "block",
             xs: "block",
           }}
-          bgGradient="radial-gradient(circle, #3889D2, #BDD4E7)">
+          bgGradient="linear(to-r, gray.300, yellow.400, pink.200)"
+        >
           <VStack alignItems={"flex-start"}>
             <Text
               fontSize={{
@@ -144,11 +213,12 @@ const Home = () => {
               variant="solid"
               marginTop={{ base: 0, lg: 0, md: 0, sm: 4, xs: 4 }}
             >
-              Connect With Us
+              Connect With Me
             </Button>
           </NextLink>
         </Flex>
       </BaseLayout>
+      <Timeline />
     </React.Fragment>
   );
 };
