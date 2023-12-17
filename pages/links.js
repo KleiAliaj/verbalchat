0e05a0f335613fbd070a8ffcbdeb8cf9e9f72b6d
KleiAliaj <40433677+KleiAliaj@users.noreply.github.com>
2023-12-17 18:13:44 +0100

    Update links.js

--- pages/links.js
+++ pages/links.js
@@ -23,26 +23,33 @@ const Links = () => {
             textIcon="https://ik.imagekit.io/ayushsoni1010/Website/notion?updatedAt=1669666487240"
             leftSpacing="4"
           />
-          <VStack
-            spacing={4}
-            width={{ base: "sm", lg: "sm", md: "sm", sm: "auto", xs: "auto" }}
-            margin={"auto"}
-            marginTop={10}
-          >
-            {profiles.map((item) => {
-              return (
-                <Button
-                  width={"full"}
-                  key={item.name}
-                  as="a"
-                  target="_blank"
-                  textAlign={"center"}
-                >
-                  {item.title}
-                </Button>
-              );
-            })}
-          </VStack>
+<VStack
+  spacing={4}
+  width={{ base: "sm", lg: "sm", md: "sm", sm: "auto", xs: "auto" }}
+  margin={"auto"}
+  marginTop={10}
+>
+  {profiles.map((item, index) => {
+    return (
+      <Button
+        width={"full"}
+        key={item.name}
+        as="a"
+        target="_blank"
+        textAlign={"center"}
+        // Apply a darker blue color to the first and seventh button
+        backgroundColor={(index === 0 || index === 6) ? "darkblue" : undefined}
+        color={(index === 0 || index === 6) ? "white" : undefined} // White text color for the first and seventh button
+        colorScheme={(index === 0 || index === 6) ? undefined : "blue"}
+        _hover={{ bg: (index === 0 || index === 6) ? "blue.800" : undefined }}
+      >
+        {item.title}
+      </Button>
+    );
+  })}
+</VStack>
+
+
         </BaseLayout>
       </main>
 
