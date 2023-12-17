c7d013295dddf3a2ca37acc0c4653a2f316c0990
KleiAliaj <40433677+KleiAliaj@users.noreply.github.com>
2023-12-17 15:00:10 +0100

    Update NavItems.js

--- components/Header/NavItems.js
+++ components/Header/NavItems.js
@@ -1,16 +1,19 @@
 import React from "react";
 import NextLink from "next/link";
 import { linkDetails } from "./__linkDetails";
-import { HStack, Button, ButtonGroup } from "@chakra-ui/react";
+import { HStack, Button, ButtonGroup, useColorMode } from "@chakra-ui/react";
 
 const NavItems = () => {
+  const { colorMode } = useColorMode(); // Access the current color mode
+  const buttonTextColor = colorMode === 'dark' ? 'white' : '#06015B'; // Set text color based on color mode
+
   return (
     <React.Fragment>
       <HStack display={{ md: "flex", sm: "none", xs: "none" }} flexDir={"row"}>
         <ButtonGroup isAttached alignSelf="center">
           {linkDetails.map((item, index) => (
             <NextLink key={index} href={item.link} passHref>
-              <Button variant={"ghost"} color="black" size="md" colorScheme="teal">
+              <Button variant="ghost" color={buttonTextColor} size="md">
                 {item.name}
               </Button>
             </NextLink>
