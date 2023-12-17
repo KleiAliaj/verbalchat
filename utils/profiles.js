a9e611ccc1a98422c127443aa20c0a8fdc3a5a60
KleiAliaj <40433677+KleiAliaj@users.noreply.github.com>
2023-12-17 17:22:51 +0100

    Update profiles.js

--- utils/profiles.js
+++ utils/profiles.js
@@ -1,215 +1,171 @@
   export const profiles = [
     {
-      "title": "List Of Prioritary Models: ",
-      "name": "",
-      "link": ""
+      title: "List Of Prioritary Models: ",
       
     },
     {
-      "title": "GPT-3 (175B)",
-      "name": "openai/GPT-3",
-      "link": "https://openai.com/gpt-3/"
+      title: "GPT-3 (175B)",
+    
     },
     {
-      "title": "GPT-3.5 Turbo (175B)",
-      "name": "openai/GPT-3",
-      "link": "https://openai.com/gpt-3/"
+      title: "GPT-3.5 Turbo (175B)",
+     
     },
     {
-      "title": "GPT-4",
-      "name": "openai/GPT-4",
-      "link": "https://openai.com/gpt-4/"
+      title: "GPT-4",
+     
     },
     {
-      "title": "GPT-4 Turbo",
-      "name": "openai/GPT-4",
-      "link": "https://openai.com/gpt-4/"
+      title: "GPT-4 Turbo",
     }
   ,
   {
-    "title": "List Of Open Source Models:",
-    "name": "",
-    "link": ""
+    title: "List Of Open Source Models:",
+   
   },
   {
     title: "Alpaca (7B)",
-    name: "togethercomputer/alpaca-7b",
-    link: "",
+
   },
   {
     title: "01-ai Yi Chat (34B)",
-    name: "zero-one-ai/Yi-34B-Chat",
-    link: "",
+    
   },
   {
     title: "Chronos Hermes (13B)",
-    name: "Austism/chronos-hermes-13b",
-    link: "",
+    
   },
   {
     title: "Code Llama Instruct (13B)",
-    name: "togethercomputer/CodeLlama-13b-Instruct",
-    link: "",
+
   },
   {
     title: "Code Llama Instruct (34B)",
-    name: "togethercomputer/CodeLlama-34b-Instruct",
-    link: "",
+
   },
   {
     title: "Code Llama Instruct (7B)",
-    name: "togethercomputer/CodeLlama-7b-Instruct",
-    link: "",
+   
   },
   {
     title: "DiscoLM Mixtral 8x7b",
-    name: "DiscoResearch/DiscoLM-mixtral-8x7b-v2",
-    link: "",
+   
   },
   {
     title: "Falcon Instruct (40B)",
-    name: "togethercomputer/falcon-40b-instruct",
-    link: "",
+  
   },
   {
     title: "Falcon Instruct (7B)",
-    name: "togethercomputer/falcon-7b-instruct",
-    link: "",
+
   },
   {
     title: "GPT-NeoXT-Chat-Base (20B)",
-    name: "togethercomputer/GPT-NeoXT-Chat-Base-20B",
-    link: "",
+   
   },
   {
     title: "LLaMA-2 Chat (13B)",
-    name: "togethercomputer/llama-2-13b-chat",
-    link: "",
+
   },
   {
     title: "LLaMA-2 Chat (70B)",
-    name: "togethercomputer/llama-2-70b-chat",
-    link: "",
+    
   },
   {
     title: "LLaMA-2 Chat (7B)",
-    name: "togethercomputer/llama-2-7b-chat",
-    link: "",
+   
   },
   {
     title: "LLaMA-2-7B-32K-Instruct (7B)",
-    name: "togethercomputer/Llama-2-7B-32K-Instruct",
-    link: "",
+  
   },
   {
     title: "Mistral (7B) Instruct v0.1",
-    name: "mistralai/Mistral-7B-Instruct-v0.1",
-    link: "",
+  
   },
   {
     title: "Mistral (7B) Instruct v0.2",
-    name: "mistralai/Mistral-7B-Instruct-v0.2",
-    link: "",
+  
   },
   {
     title: "Mixtral-8x7B Instruct",
-    name: "mistralai/Mixtral-8x7B-Instruct-v0.1",
-    link: "",
+ 
   },
   {
     title: "MythoMax-L2 (13B)",
-    name: "Gryphe/MythoMax-L2-13b",
-    link: "",
+
   },
   {
     title: "Nous Hermes LLaMA-2 (7B)",
-    name: "NousResearch/Nous-Hermes-llama-2-7b",
-    link: "",
+ 
   },
   {
     title: "Nous Hermes Llama-2 (13B)",
-    name: "NousResearch/Nous-Hermes-Llama2-13b",
-    link: "",
+  
   },
   {
     title: "Nous Hermes Llama-2 (70B)",
-    name: "NousResearch/Nous-Hermes-Llama2-70b",
-    link: "",
+    
   },
   {
     title: "Nous Capybara v1.9 (7B)",
-    name: "NousResearch/Nous-Capybara-7B-V1p9",
-    link: "",
+
   },
   {
     title: "OpenChat 3.5 1210 (7B)",
-    name: "openchat/openchat-3.5-1210",
-    link: "",
+   
   },
   {
     title: "OpenHermes-2-Mistral (7B)",
-    name: "teknium/OpenHermes-2-Mistral-7B",
-    link: "",
+
   },
   {
     title: "OpenHermes-2.5-Mistral (7B)",
-    name: "teknium/OpenHermes-2p5-Mistral-7B",
-    link: "",
+  
   },
   {
     title: "OpenOrca Mistral (7B) 8K",
-    name: "Open-Orca/Mistral-7B-OpenOrca",
-    link: "",
+  
   },
   {
     title: "Platypus2 Instruct (70B)",
-    name: "garage-bAInd/Platypus2-70B-instruct",
-    link: "",
+    
   },
   {
     title: "Pythia-Chat-Base (7B)",
-    name: "togethercomputer/Pythia-Chat-Base-7B-v0.16",
-    link: "",
+   
   },
   {
     title: "Qwen-Chat (7B)",
-    name: "togethercomputer/Qwen-7B-Chat",
-    link: "",
+   
   },
   {
     title: "RedPajama-INCITE Chat (3B)",
-    name: "togethercomputer/RedPajama-INCITE-Chat-3B-v1",
-    link: "",
+ 
   },
   {
     title: "RedPajama-INCITE Chat (7B)",
-    name: "togethercomputer/RedPajama-INCITE-7B-Chat",
-    link: "",
+  
   },
   {
     title: "SOLAR v0 (70B)",
-    name: "upstage/SOLAR-0-70b-16bit",
-    link: "",
+    
   },
   {
     title: "StripedHyena Nous (7B)",
-    name: "togethercomputer/StripedHyena-Nous-7B",
-    link: "",
+    
   },
   {
     title: "Vicuna v1.5 (7B)",
-    name: "lmsys/vicuna-7b-v1.5",
-    link: "",
+   
   },
   {
     title: "Vicuna v1.5 (13B)",
-    name: "lmsys/vicuna-13b-v1.5",
-    link: "",
+  
   },
   {
     title: "Vicuna v1.5 16K (13B)",
-    name: "lmsys/vicuna-13b-v1.5-16k",
-    link: "",
+
   },
   ];
 
